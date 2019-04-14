6a9353c5ed2048896367e5ade223cd2955c178da
Maciej Piorun <maciej.piorun2@gmail.com>
2019-04-14 12:13:05 +0200

    Added hello world spring app

---
+++ Spring/src/main/java/com/piorun/shape/AppStarter.java
@@ -0,0 +1,12 @@
+package com.piorun.shape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStarter.class, args);
+    }
+}
